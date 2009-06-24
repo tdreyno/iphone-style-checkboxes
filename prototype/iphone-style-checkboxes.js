9ca4c87ce20d1b59894a4bb306d3c1bd5e43a975
Elijah Miller <elijah.miller@gmail.com>
2009-06-25 04:34:38 +0800

    Working version using Scriptaculous.
    Signed-off-by: Thomas Reynolds <tdreyno@gmail.com>

--- prototype/iphone-style-checkboxes.js
+++ prototype/iphone-style-checkboxes.js
@@ -1,45 +1,47 @@
 $.iphoneStyle = {
-  defaults: { checkedLabel: 'ON', uncheckedLabel: 'OFF', background: '#fff' }
+  defaults: { 
+    checkedLabel: 'ON', uncheckedLabel: 'OFF', background: '#fff',
+    containerClass:    'iPhoneCheckContainer',
+    labelOnClass:      'iPhoneCheckLabelOn',
+    labelOffClass:     'iPhoneCheckLabelOff',
+    handleClass:       'iPhoneCheckHandle',
+    handleSliderClass: 'iPhoneCheckHandleSlider'
+  }
 }
 
-$.fn.iphoneStyle = function(selector_or_elem, options) {
-  options = $.extend($.iphoneStyle.defaults, options);
+var iPhoneStyle = function(selector_or_elem, options) {
+  options = Object.extend($.iphoneStyle.defaults, options);
   
-  return $$(selector_or_elem).each(function() {
-    var elem = $$(this);
+  return $$(selector_or_elem).each(function(elem) {
     
     if (!elem.match('input[type=checkbox]'))
       return;
     
     elem.setOpacity(0);
-    elem.wrap('<div class="container" />');
-    elem.insert({ after: '<div class="handle"><div class="bg" style="background: ' + options.background + '"/><div class="slider" /></div>' })
-        .insert({ after: '<label class="off">'+ options.uncheckedLabel + '</label>' })
-        .insert({ after: '<label class="on">' + options.checkedLabel   + '</label>' });
+    elem.wrap('div', { 'class': options.containerClass});
+    elem.insert({ 'after': '<div class="' + options.handleClass + '"><div class="' + options.handleSliderClass + '" /></div>' })
+        .insert({ 'after': '<label class="' + options.labelOffClass + '">'+ options.uncheckedLabel + '</label>' })
+        .insert({ 'after': '<label class="' + options.labelOnClass + '">' + options.checkedLabel   + '</label>' });
     
-    var handle    = elem.adjacent('.handle'),
-        handlebg  = handle.down('.bg'),
-        offlabel  = elem.adjacent('.off'),
-        onlabel   = elem.adjacent('.on'),
-        container = elem.parent('.container'),
+    var handle    = elem.up().down('.' + options.handleClass),
+        handlebg  = handle.down('.' + options.handleBGClass),
+        offlabel  = elem.adjacent('.' + options.labelOffClass).first(),
+        onlabel   = elem.adjacent('.' + options.labelOnClass).first(),
+        container = elem.up('.' + options.containerClass),
         rightside = container.getWidth() - 39;
+        
     
     container.observe('mouseup', function() {
-      var is_onstate = (handle.position().left <= 0);
-          new_left   = (is_onstate) ? rightside : 0,
-          bgleft     = (is_onstate) ? 34 : 0;
-
-      handlebg.hide();
-      handle.animate({ left: new_left }, 100, function() {
-        handlebg.setStyle({ left: bgleft }).show();
-      });
+      var is_onstate = (elem.checked);
+    
+      new Effect.Tween(null, (is_onstate) ? 1 : 0, (is_onstate) ? 0 : 1, { duration: 0.2 }, function(p) { handle.setStyle({ left: p * rightside + 'px' }) });
       
       if (is_onstate) {
-        offlabel.animate({ opacity: 0 }, 200);
-        onlabel.animate({ opacity: 1 }, 200);
+        offlabel.appear({ duration: 0.2 });
+        onlabel.fade({ duration: 0.2 });
       } else {
-        offlabel.animate({ opacity: 1 }, 200);
-        onlabel.animate({ opacity: 0 }, 200);
+        offlabel.fade({ duration: 0.2 });
+        onlabel.appear({ duration: 0.2 });
       }
       
       elem.writeAttribute('checked', !is_onstate);
@@ -47,16 +49,15 @@ $.fn.iphoneStyle = function(selector_or_elem, options) {
     });
     
     // Disable text selection
-    $(container, onlabel, offlabel, handle).observe('mousedown', function(e) { Event.stop(e); return false; });
-    if ($.browser.ie)
-      $(container, onlabel, offlabel, handle).observe('startselect', function(e) { Event.stop(e); return false; });
+    [container, onlabel, offlabel, handle].invoke('observe', 'mousedown', function(e) { Event.stop(e); return false; });
+    if (Prototype.Browser.IE)
+      [container, onlabel, offlabel, handle].invoke('observe', 'startselect', function(e) { Event.stop(e); return false; });
     
     // initial load
     if (elem.match(':checked')) {
       offlabel.setOpacity(0);
       onlabel.setOpacity(1);
-      handle.setStyle({ left: rightside });
-      handlebg.setStyle({ left: 34 });
+      handle.setStyle({ left: rightside + 'px'});
     }
   });
 };
