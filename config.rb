bf74850b332a15a2efaf9519080b7d18eba9b568
Thomas Reynolds <tdreyno@gmail.com>
2010-08-17 15:29:47 -0700

    use the sass extension

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = ""
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
+output_style = :nested
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+line_comments = false
