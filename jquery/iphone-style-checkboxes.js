29b8ee7d2e90cf1ab9f485de1a9ebb37464dbddc
Martin Tóth <ezimir@gmail.com>
2010-12-14 13:30:36 +0100

    Only animate one element on page (not every styled checkbox).

--- jquery/iphone-style-checkboxes.js
+++ jquery/iphone-style-checkboxes.js
@@ -7,21 +7,21 @@
 // Constructor
 $[iphoneStyle] = function(elem, options) {
   this.$elem = $(elem);
-  
+
   // Import options into instance variables
   var obj = this;
   $.each(options, function(key, value) {
     obj[key] = value;
   });
-  
+
   // Initialize the control
   this.wrapCheckboxWithDivs();
   this.attachEvents();
   this.disableTextSelection();
-  
+
   if (this.resizeHandle)    { this.optionallyResize('handle'); }
   if (this.resizeContainer) { this.optionallyResize('container'); }
-  
+
   this.initialPosition();
 };
 
@@ -30,24 +30,24 @@ $.extend($[iphoneStyle].prototype, {
   wrapCheckboxWithDivs: function() {
     this.$elem.wrap('<div class="' + this.containerClass + '" />');
     this.container = this.$elem.parent();
-    
+
     this.offLabel  = $('<label class="'+ this.labelOffClass +'">' +
                          '<span>'+ this.uncheckedLabel +'</span>' +
                        '</label>').appendTo(this.container);
     this.offSpan   = this.offLabel.children('span');
-    
+
     this.onLabel   = $('<label class="'+ this.labelOnClass +'">' +
                          '<span>'+ this.checkedLabel +'</span>' +
                        '</label>').appendTo(this.container);
     this.onSpan    = this.onLabel.children('span');
-    
+
     this.handle    = $('<div class="' + this.handleClass + '">' +
                          '<div class="' + this.handleRightClass + '">' +
                            '<div class="' + this.handleCenterClass + '" />' +
                          '</div>' +
                        '</div>').appendTo(this.container);
   },
-  
+
   // Disable IE text selection, other browsers are handled in CSS
   disableTextSelection: function() {
     if (!$.browser.msie) { return; }
@@ -57,44 +57,46 @@ $.extend($[iphoneStyle].prototype, {
       $(el).attr("unselectable", "on");
     });
   },
-  
+
   // Automatically resize the handle or container
   optionallyResize: function(mode) {
     var onLabelWidth  = this.onLabel.width(),
         offLabelWidth = this.offLabel.width();
-        
+
     if (mode == 'container') {
       var newWidth = (onLabelWidth > offLabelWidth) ? onLabelWidth : offLabelWidth;
-      newWidth += this.handle.width() + 15; 
-    } else { 
+      newWidth += this.handle.width() + 15;
+    } else {
       var newWidth = (onLabelWidth < offLabelWidth) ? onLabelWidth : offLabelWidth;
     }
-    
+
     this[mode].css({ width: newWidth });
   },
-  
+
   attachEvents: function() {
     var obj = this;
-    
+
     // A mousedown anywhere in the control will start tracking for dragging
     this.container
-      .bind('mousedown touchstart', function(event) {          
+      .bind('mousedown touchstart', function(event) {
         event.preventDefault();
-        
+
         if (obj.$elem.is(':disabled')) { return; }
-          
+
         var x = event.pageX || event.originalEvent.changedTouches[0].pageX;
         $[iphoneStyle].currentlyClicking = obj.handle;
         $[iphoneStyle].dragStartPosition = x;
         $[iphoneStyle].handleLeftOffset  = parseInt(obj.handle.css('left'), 10) || 0;
+        $[iphoneStyle].dragStartedOn = obj.$elem;
       })
-    
+
       // Utilize event bubbling to handle drag on any element beneath the container
       .bind('iPhoneDrag', function(event, x) {
         event.preventDefault();
-        
+
         if (obj.$elem.is(':disabled')) { return; }
-        
+        if (obj.$elem != $[iphoneStyle].dragStartedOn) { return; }
+
         var p = (x + $[iphoneStyle].handleLeftOffset - $[iphoneStyle].dragStartPosition) / obj.rightSide;
         if (p < 0) { p = 0; }
         if (p > 1) { p = 1; }
@@ -103,11 +105,11 @@ $.extend($[iphoneStyle].prototype, {
         obj.offSpan.css({ marginRight: -p * obj.rightSide });
         obj.onSpan.css({ marginLeft: -(1 - p) * obj.rightSide });
       })
-    
+
         // Utilize event bubbling to handle drag end on any element beneath the container
       .bind('iPhoneDragEnd', function(event, x) {
         if (obj.$elem.is(':disabled')) { return; }
-        
+
         if ($[iphoneStyle].dragging) {
           var p = (x - $[iphoneStyle].dragStartPosition) / obj.rightSide;
           obj.$elem.attr('checked', (p >= 0.5));
@@ -119,7 +121,7 @@ $.extend($[iphoneStyle].prototype, {
         $[iphoneStyle].dragging = null;
         obj.$elem.change();
       });
-  
+
     // Animate when we get a change event
     this.$elem.change(function() {
       if (obj.$elem.is(':disabled')) {
@@ -128,7 +130,7 @@ $.extend($[iphoneStyle].prototype, {
       } else {
         obj.container.removeClass(obj.disabledClass);
       }
-      
+
       var new_left = obj.$elem.attr('checked') ? obj.rightSide : 0;
 
       obj.handle.animate({         left: new_left },                 obj.duration);
@@ -137,7 +139,7 @@ $.extend($[iphoneStyle].prototype, {
       obj.onSpan.animate({   marginLeft: new_left - obj.rightSide }, obj.duration);
     });
   },
-  
+
   // Setup the control's inital position
   initialPosition: function() {
     this.offLabel.css({ width: this.container.width() - 5 });
@@ -153,7 +155,7 @@ $.extend($[iphoneStyle].prototype, {
       this.onLabel.css({ width: 0 });
       this.onSpan.css({ marginLeft: -this.rightSide });
     }
-    
+
     if (this.$elem.is(':disabled')) {
       this.container.addClass(this.disabledClass);
     }
@@ -163,13 +165,13 @@ $.extend($[iphoneStyle].prototype, {
 // jQuery-specific code
 $.fn[iphoneStyle] = function(options) {
   var checkboxes = this.filter(':checkbox');
-  
+
   // Fail early if we don't have any checkboxes passed in
   if (!checkboxes.length) { return this; }
-  
+
   // Merge options passed in with global defaults
   var opt = $.extend({}, $[iphoneStyle].defaults, options);
-  
+
   checkboxes.each(function() {
     $(this).data(iphoneStyle, new $[iphoneStyle](this, opt));
   });
@@ -180,28 +182,28 @@ $.fn[iphoneStyle] = function(options) {
       .bind('mousemove touchmove', function(event) {
         if (!$[iphoneStyle].currentlyClicking) { return; }
         event.preventDefault();
-        
+
         var x = event.pageX || event.originalEvent.changedTouches[0].pageX;
         if (!$[iphoneStyle].dragging &&
-            (Math.abs($[iphoneStyle].dragStartPosition - x) > opt.dragThreshold)) { 
-          $[iphoneStyle].dragging = true; 
+            (Math.abs($[iphoneStyle].dragStartPosition - x) > opt.dragThreshold)) {
+          $[iphoneStyle].dragging = true;
         }
-    
+
         $(event.target).trigger('iPhoneDrag', [x]);
       })
 
       // When the mouse comes up, leave drag state
-      .bind('mouseup touchend', function(event) {        
+      .bind('mouseup touchend', function(event) {
         if (!$[iphoneStyle].currentlyClicking) { return; }
         event.preventDefault();
-    
+
         var x = event.pageX || event.originalEvent.changedTouches[0].pageX;
         $($[iphoneStyle].currentlyClicking).trigger('iPhoneDragEnd', [x]);
       });
-      
+
     $[iphoneStyle].initComplete = true;
   }
-  
+
   return this;
 }; // End of $.fn[iphoneStyle]
 
