f25e0bb79a5c0c1c1628f101e89150b048676bd5
Thomas Reynolds <me@tdreyno.com>
2013-01-23 11:55:47 -0800

    1.9 compat

--- iphone-style-checkboxes/iphone-style-checkboxes.js
+++ iphone-style-checkboxes/iphone-style-checkboxes.js
@@ -1,6 +1,26 @@
 (function() {
-  var iOSCheckbox,
+  var iOSCheckbox, matched, userAgent,
     __slice = Array.prototype.slice;
+
+  if ($.browser == null) {
+    userAgent = navigator.userAgent || "";
+    jQuery.uaMatch = function(ua) {
+      var match;
+      ua = ua.toLowerCase();
+      match = /(chrome)[ \/]([\w.]+)/.exec(ua) || /(webkit)[ \/]([\w.]+)/.exec(ua) || /(opera)(?:.*version)?[ \/]([\w.]+)/.exec(ua) || /(msie) ([\w.]+)/.exec(ua) || ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+))?/.exec(ua) || [];
+      return {
+        browser: match[1] || "",
+        version: match[2] || "0"
+      };
+    };
+    matched = jQuery.uaMatch(userAgent);
+    jQuery.browser = {};
+    if (matched.browser) {
+      jQuery.browser[matched.browser] = true;
+      jQuery.browser.version = matched.version;
+    }
+    if (jQuery.browser.webkit) jQuery.browser.safari = true;
+  }
 
   iOSCheckbox = (function() {
 
